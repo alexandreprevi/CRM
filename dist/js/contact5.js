63ac47fa197edbf9562572e993e88b379e8fa6d3
Guilin Li <liguilin2012@gmail.com>
2019-10-25 17:48:15 +0200

    made notes and fixed mall bug

--- dist/js/contact5.js
+++ dist/js/contact5.js
@@ -6,57 +6,41 @@ class ContactList {
   }
 
   addContact(companyName, companyWeb, companyAddress, personName, personTel, personEmail) {
+    this.contact_id++;
     var contact = new ContactItem(companyName, companyWeb, companyAddress, personName, personTel, personEmail, this.contact_id);
     this.contacts.push(contact);
-    //sorterar objekten i arrayen functionen compare finns längst ner
-    //this.contacts.sort((a, b) => a.companyName.localeCompare(b.companyName)); ///sort()here also doesn't work corretly
     this.render();
-    this.contact_id++; // PROBLEM: del 1 av 3: Id används för att skilja på kontaket, vilket ärr jätte bra, men scrolla ner
   }
 
-  // fyller contactarea
-  render() {      
+  // fill contact list to contact area
+  render() {
     var list = document.getElementById("contacts_area");
     list.innerHTML = "";
-    var arr = [];
-    let sortedcontactlist = this.contacts.sort(compare);
+    let sortedcontactlist = this.contacts.sort(compare); //sort object in array function compare located at the end of code
+
     for (let currentContact of sortedcontactlist) {
-     console.log("Inside render with id: " + currentContact.id);
-      //  var newContactDiv = document.createElement("div");
-      //newContactDiv.id = "newContact-" + currentContact.id;
       var new_contactName = document.createElement("p");
       new_contactName.id = "new_contactName-" + currentContact.id;
-      //new_contactName.id = currentContact.id.toString();
       new_contactName.class = "new_contactName";
       new_contactName.innerHTML = currentContact.companyName + " - " + currentContact.personName;
 
       list.appendChild(new_contactName);
 
-      arr.push(new_contactName.innerHTML);
-      console.log(arr);
-
+      //add click EventListener to each item on the contact list, when click on it, will show details of that contact
       new_contactName.addEventListener('click', function() {
-
-        //såja
-        for(let index = 0; index < sortedcontactlist.length;index++){
-          for(let prop in sortedcontactlist[index]){
-
-            if(currentContact.id == sortedcontactlist[index][prop]){
-              showDetails(index); //del 1.5 / 3: id används som parameter
+        for (let index = 0; index < sortedcontactlist.length; index++) {
+          for (let prop in sortedcontactlist[index]) {
+            if (currentContact.id == sortedcontactlist[index][prop]) {
+              showDetails(index); //showDetails function is located at the end of code
             }
-
           }
         }
-        
-       });
-
+      });
     }
   }
 
-  // display details of contact
-  render2(contact_index) { //del 2 / 3: id används som index!! Här e problemet, eftrsom Id har bytt plats tyvärr! så id != index!
-    console.log("Inside render2 with id: " + contact_index);
-
+  // fill details of contact to contact_page area
+  render2(contact_index) {
     var contact_page = document.getElementById("contact_page");
     contact_page.innerHTML = "";
 
@@ -77,12 +61,11 @@ class ContactList {
     new_personTel.id = "new_personTel-" + contact_index;
     var new_personEmail = document.createElement("p");
     new_personEmail.id = "new_personEmail-" + contact_index;
-    // del 3/3: fel info matas in !
+
     new_companyName.innerHTML = "Company Name: " + this.contacts[contact_index].companyName;
     new_companyWeb.innerHTML = "Company Website: " + this.contacts[contact_index].companyWeb;
     new_companyAddress.innerHTML = "Company Address: " + this.contacts[contact_index].companyAddress;
     new_personName.innerHTML = "Contact's name: " + this.contacts[contact_index].personName;
-
     new_personTel.innerHTML = "Contact's PhoneNumber: " + this.contacts[contact_index].personTel;
     new_personEmail.innerHTML = "Contact's Email: " + this.contacts[contact_index].personEmail;
 
@@ -109,45 +92,30 @@ class ContactItem {
   }
 }
 
-function showDetails(contactId) {
-  console.log("in it: " + contactId);
-  contact_list.render2(contactId);
-}
-
 var contact_list = new ContactList();
 document.addEventListener("DOMContentLoaded", function(event) {
-
   var addNewContact = document.getElementById("addNewContact");
   document.getElementById("btn_addContact").addEventListener("click", function(e) {
-    //creatNewContact();
     addNewContact.style.display = "inline";
   });
 
-
   document.getElementById("btn_addContact_confirm").addEventListener("click", function(e) {
-
     var company_name = document.getElementById("company_name").value;
     var company_web = document.getElementById("company_web").value;
     var company_address = document.getElementById("company_address").value;
     var person_name = document.getElementById("person_name").value;
     var person_tel = document.getElementById("person_tel").value;
     var person_email = document.getElementById("person_email").value;
-
     addNewContact.style.display = "none";
     contact_list.addContact(company_name, company_web, company_address, person_name, person_tel, person_email);
-    console.log(contact_list.contacts);
-    //contact_list.contacts.sort(compare);
-   //contact_list.contacts.sort((a, b) => a.companyName.localeCompare(b.companyName));//It is wrong oder when sort() here
-
   });
-
 });
 
+// this function is to sort contact list alphabetically, will call this function inside render();
 function compare(a, b) {
   // Use toUpperCase() to ignore character casing
   const companyA = a.companyName.toUpperCase();
   const companyB = b.companyName.toUpperCase();
-
   let comparison = 0;
   if (companyA > companyB) {
     comparison = 1;
@@ -155,4 +123,9 @@ function compare(a, b) {
     comparison = -1;
   }
   return comparison;
+}
+
+// this function is to show details of contact on the contact-list, will call this function inside render()
+function showDetails(contactId) {
+  contact_list.render2(contactId);
 }
