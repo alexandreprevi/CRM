b8c3aef3e005fa5cbd45a99fdb330b6eedb7cd8b
Guilin Li <liguilin2012@gmail.com>
2019-11-02 00:40:08 +0100

    combined/tested contact to master

--- dist/js/contact.js
+++ dist/js/contact.js
@@ -93,7 +93,7 @@ class Calendar {
                 event.date < today ?   previousEventsTable.appendChild(tr) : upcomingEventsTable.appendChild(tr);
             }
         }
-        
+
     } */
 
     renderThisEvent(event) {
@@ -175,11 +175,11 @@ $.get("http://5daef5cbf2946f001481d066.mockapi.io/events", function (data) {
 
     for (let i = 0; i <eventsInPreviousEventsTable.length; i++){
         let event = eventsInPreviousEventsTable[i];
-    
+
         if (event) {
-            
+
             txtValue = event.textContent || event.innerText;
-        
+
             if (txtValue.toUpperCase().indexOf(filter) > -1) {
                 eventsInPreviousEventsTable[i].style.display = "";
             } else {
@@ -190,9 +190,9 @@ $.get("http://5daef5cbf2946f001481d066.mockapi.io/events", function (data) {
 
     for (let i = 0; i <eventsInUpcomingEventsTable.length; i++){
         let event = eventsInUpcomingEventsTable[i];
-        
+
         if (event) {
-            
+
             txtValue = event.textContent || event.innerText;
 
             if (txtValue.toUpperCase().indexOf(filter) > -1) {
@@ -225,19 +225,15 @@ let eventsBtn = document.getElementById("contact-events-button");
 let notesBtn = document.getElementById("contact-notes-button");
 
 
-
-
-
 // start display (event listener on name of contact on the list)
 contactList.style.display = "flex";
 contactHeader.style.display = "flex";
 contactDetails.style.display = "none";
 
 
-
 // event listeners
-
-contactList.addEventListener("click", function (contact) {
+contactList.addEventListener("click", displayDetails);
+function displayDetails(contact) {
 
     if (contact.target.classList.contains("contact-name")) {
         contactList.style.display = "none";
@@ -247,9 +243,13 @@ contactList.addEventListener("click", function (contact) {
         contactNotesDisplay.style.display = "none";
 
         document.getElementById("contact-display-name").innerHTML = contact.target.innerHTML;
-    }
-})
+        var nodes = Array.prototype.slice.call( document.getElementById('contact-list').children ),
+            ref = contact.target;
 
+        console.log( nodes.indexOf( ref ) );
+        showDetails(nodes.indexOf(ref).toString());
+    }
+}
 
 backBtn.addEventListener("click", function () {
     contactList.style.display = "flex";
@@ -257,11 +257,14 @@ backBtn.addEventListener("click", function () {
     contactDetails.style.display = "none";
 });
 
-infoBtn.addEventListener("click", function () {
+infoBtn.addEventListener("click", function(contact) {
+
     contactInfoDisplay.style.display = "flex";
     contactEventsDisplay.style.display = "none";
     contactNotesDisplay.style.display = "none";
+    displayDetails(contact)
 });
+
 eventsBtn.addEventListener("click", function () {
     contactInfoDisplay.style.display = "none";
     contactEventsDisplay.style.display = "flex";
@@ -272,3 +275,184 @@ notesBtn.addEventListener("click", function () {
     contactEventsDisplay.style.display = "none";
     contactNotesDisplay.style.display = "flex";
 });
+
+// ======code from branch LI -contact===========
+class ContactList {
+  constructor() {
+    this.contacts = [];
+    this.contactDetails = [];
+    this.contact_id = 0;
+  }
+
+  addContact(companyName, companyWeb, companyAddress, firstName,lastName, tel, email) {
+    this.contact_id++;
+    var contact = new ContactItem(companyName, companyWeb, companyAddress, firstName,lastName, tel, email,this.contact_id);
+    this.contacts.push(contact);
+    this.render();
+  }
+
+
+  // fill contact list to contact area
+  render() {
+    var contactList = document.getElementById("contact-list");
+    contactList.innerHTML = "";
+    let sortedcontactlist = this.contacts.sort(compare); //sort object in array function compare located at the end of code
+
+    for (let currentContact of sortedcontactlist) {
+      var contactName = document.createElement("p");
+      contactName.id = "contact-name-" + currentContact.id;
+      contactName.className = "contact-name";
+      contactName.innerHTML = currentContact.companyName + " - " + currentContact.firstName +" " +currentContact.lastName;
+      contactList.appendChild(contactName);
+    }
+  }
+
+  // fill details of contact to contact-details area
+  render2(contact_index) {
+  //  var contactDetails = document.getElementById("contact-details");
+  //  contactDetails.innerHTML = "";
+    var contactDetailsDisplayInfo=document.getElementById("contact-details-display-info");
+    contactDetailsDisplayInfo.innerHTML="";
+
+      var contactDiv = document.createElement("div");
+      contactDiv.id = "contact-" + contact_index;
+
+      var companyName = document.createElement("p");
+      companyName.id = "companyName-" + contact_index;
+      var companyWeb = document.createElement("p");
+      companyWeb.id = "companyWeb-" + contact_index;
+      var companyAddress = document.createElement("p");
+      companyAddress.id = "companyAddress-" + contact_index;
+      var firstName = document.createElement("p");
+      firstName.id = "firstName-" + contact_index;
+      var lastName = document.createElement("p");
+      lastName.id = "lastName-" + contact_index;
+      var tel = document.createElement("p");
+      tel.id = "tel-" + contact_index;
+      var email = document.createElement("p");
+      email.id = "email-" + contact_index;
+
+      companyName.innerHTML = "Company Name: " + this.contacts[contact_index].companyName;
+      companyWeb.innerHTML = "Company Website: " + this.contacts[contact_index].companyWeb;
+      companyAddress.innerHTML = "Company Address: " + this.contacts[contact_index].companyAddress;
+      firstName.innerHTML = "First name: " + this.contacts[contact_index].firstName;
+      lastName.innerHTML = "Last name: " + this.contacts[contact_index].lastName;
+      tel.innerHTML = "PhoneNumber: " + this.contacts[contact_index].tel;
+      email.innerHTML = " Email: " + this.contacts[contact_index].email;
+
+      contactDiv.appendChild(companyName);
+      contactDiv.appendChild(companyWeb);
+      contactDiv.appendChild(companyAddress);
+      contactDiv.appendChild(firstName);
+      contactDiv.appendChild(lastName);
+      contactDiv.appendChild(tel);
+      contactDiv.appendChild(email);
+
+      contactDetailsDisplayInfo.appendChild(contactDiv);
+  }
+}
+
+class ContactItem {
+  constructor(companyName = "", companyWeb = "", companyAddress = "", firstName = "",lastName = "", tel = "", email = "", id) {
+    this.companyName = companyName;
+    this.companyWeb = companyWeb;
+    this.companyAddress = companyAddress;
+    this.firstName = firstName;
+    this.lastName =lastName;
+    this.tel = tel;
+    this.email = email;
+    this.id = id;
+  }
+}
+
+var contact_list = new ContactList();
+var addNewContact = document.getElementById("addNewContact");
+//var contactList = document.getElementById("contact_list");
+//var contactDetails = document.getElementById("contact-details");
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    document.getElementById("btn_addContact").addEventListener("click", function(e) {
+    addNewContact.style.display = "inline";
+  });
+
+  document.getElementById("btn_addContact_confirm").addEventListener("click", function(e) {
+    var company_name = document.getElementById("company_name").value;
+    var company_web = document.getElementById("company_web").value;
+    var company_address = document.getElementById("company_address").value;
+    var first_name = document.getElementById("first_name").value;
+    var last_name = document.getElementById("last_name").value;
+    var tel = document.getElementById("tel").value;
+    var email = document.getElementById("email").value;
+    addNewContact.style.display = "none";
+    contact_list.addContact(company_name, company_web, company_address, first_name, last_name, tel, email);
+    additem();
+  });
+  document.getElementById("btn_addContact_cancel").addEventListener("click", function(e) {
+    addNewContact.style.display = "none";
+  });
+});
+
+// MockiAPI
+$.get("http://5daef5cbf2946f001481d066.mockapi.io/contacts", function(data){
+   for (let contact of data){
+       contact_list.contacts.push(contact);
+   }
+   contact_list.render();
+});
+
+function additem(){
+    $.ajax({
+  method: "POST",
+  url: "http://5daef5cbf2946f001481d066.mockapi.io/contacts",
+  data: {
+  companyName: document.getElementById("company_name").value,
+  companyWeb: document.getElementById("company_web").value,
+  companyAddress: document.getElementById("company_address").value,
+  firstName:document.getElementById("first_name").value,
+  lastName: document.getElementById("last_name").value,
+  tel:  document.getElementById("tel").value,
+  email: document.getElementById("email").value
+  }
+})
+  .done(function( msg ) {
+    console.log( msg );
+  });
+}
+
+
+
+// this function is to sort contact list alphabetically, will call this function inside render();
+function compare(a, b) {
+  // Use toUpperCase() to ignore character casing
+  const companyA = (a.companyName.toUpperCase()+" - "+a.firstName.toUpperCase() +a.lastName.toUpperCase());
+  const companyB = (b.companyName.toUpperCase()+" - "+b.firstName.toUpperCase() +b.lastName.toUpperCase());
+  let comparison = 0;
+  if (companyA > companyB) {
+    comparison = 1;
+  } else if (companyA < companyB) {
+    comparison = -1;
+  }
+  return comparison;
+}
+
+// this function is to show details of contact on the contact-list, will call this function inside render()
+function showDetails(contactId) {
+  contact_list.render2(contactId);
+}
+
+// contact list page -search funtion
+document.getElementById("searchbar").addEventListener("keyup", search_contact);
+function search_contact() {
+    let input = document.getElementById('searchbar').value
+    input=input.toLowerCase();
+    let x = document.getElementById("contact-list")
+
+    for (let i = 0; i <x.children.length; i++) {
+        if (!x.children[i].innerHTML.toLowerCase().includes(input)) {
+            x.children[i].style.display="none";
+        }
+        else {
+            x.children[i].style.display="inline";
+        }
+    }
+}
